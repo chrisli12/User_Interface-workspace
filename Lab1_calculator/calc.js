e03007eed7732a27d8a9f6445484d91c5ae2d49f
Chris Li <lich.chris0324@gmail.com>
2019-09-26 01:27:05 -0400

    ch

---
+++ Lab1_calculator/calc.js
@@ -0,0 +1,34 @@
+window.onload = function(){
+  var add = document.getElementById
+
+
+}
+
+
+
+
+
+
+function myFunction() {
+  //document.getElementById("demo").style.backgroundColor = "red";
+  document.getElementById("calcDisplay").innerHTML++;
+}
+
+function myFunction2() {
+ 
+  var a=parseInt(document.getElementById("foo").value);
+
+  switch (true){
+	case (a>89):
+		answer="A+";
+		break;
+	case (a>79):
+		answer="A";
+		break;
+	default:
+		answer="F";
+  }
+
+  document.getElementById("output").innerHTML=answer;
+
+}
